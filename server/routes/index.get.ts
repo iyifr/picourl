bce0a5dbc8f77175a766154d816fbaaa0d1f5b87
iyifr <adekileiyimide@gmail.com>
2025-03-07 17:23:07 +0100

    feat: update styles

--- server/routes/index.get.ts
+++ server/routes/index.get.ts
@@ -2,14 +2,13 @@ import useHash from '~/plugins/hash'
 import { edge, keyStorage } from '~/plugins/plugs'
 
 export default eventHandler(async (event) => {
-	let url = getQuery(event).url as string
+	let url = getQuery(event).url as string | null
 
 	if (url) {
 		const hashkey = useHash().update(url).digest('hex').slice(0, 6)
 		await keyStorage.setItem(hashkey, url)
-		url = `localhost:3000/${hashkey}`
-	} else {
-		url = undefined
+		const requestUrl = getRequestURL(event)
+		url = `${requestUrl.origin}/${hashkey}`
 	}
 
 	const data = { username: 'virk', url }
